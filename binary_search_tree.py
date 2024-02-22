93f2d385a9f0259eafec0f2c9d9515d9d6a3b0e5
Remingusu <lewee.m33@gmail.com>
2024-02-22 20:28:33 +0100

    Add features, Docstring update, Node attributes modification
    - Features:
        search a node in the tree
        check if the node is a leaf
        starting the methode to delete a node
        balancing tree in wait
    
    - Docstrings:
        Add docstrings for new features
        Update param type
    
    - Node modifications:
        Add the parent of the node
        Add the side of the node (left or right)
    
        Update insertion of node method with this new attributes

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -1,19 +1,16 @@
 class Node:
-    def __init__(self, value, l_subtree=None, r_subtree=None):
-        self.value, self.l_subtree, self.r_subtree = value, l_subtree, r_subtree
+    def __init__(self, value, l_subtree=None, r_subtree=None, parent=None, side=0):
+        self.value, self.l_subtree, self.r_subtree, self.parent, self.side = value, l_subtree, r_subtree, parent, side
 
     def __str__(self):
         return f"({self.l_subtree} - {self.value} - {self.r_subtree})"
-        # return f"  -> {self.l_subtree}\n"\
-        #        f"{self.value}\n"\
-        #        f"  -> {self.r_subtree}"
 
 
 class Tree:
     def __init__(self, root):
         """
-        Create the tree with the given root
-        :param root: Node()
+        Generate a tree with the given root
+        :param Node() root: the first node of the tree
         """
         self.root = root
         self.current_node = root
@@ -21,11 +18,12 @@ class Tree:
     def insert_node(self, node):
         """
         Add a node to the tree
-        :param node: Node()
-        :return: None
+        :param Node() node: the node to insert
         """
         if self.current_node.value > node.value:
             if self.current_node.l_subtree is None:
+                node.parent = self.current_node
+                node.size = 0
                 self.current_node.l_subtree = node
             else:
                 self.current_node = self.current_node.l_subtree
@@ -33,6 +31,8 @@ class Tree:
 
         else:
             if self.current_node.r_subtree is None:
+                node.size = 1
+                node.parent = self.current_node
                 self.current_node.r_subtree = node
             else:
                 self.current_node = self.current_node.r_subtree
@@ -40,14 +40,63 @@ class Tree:
 
         self.current_node = self.root
 
-    def delete_node(self):
+    def delete_node(self, node_value):
+        """
+        Delete a node from the tree
+        :param int node_value: the value of the node to delete | The value need to be in the tree
+        """
+        if self.is_leaf(node_value):
+            node = self.search_node(node_value, True)[1]
+            node_side = node.side
+            node_parent = node.parent
+            if node_side == 0:
+                node_parent.l_subtree = None
+            else:
+                node_parent.r_subtree = None
+        # in work
+        # https://www.geeksforgeeks.org/deletion-in-binary-search-tree/
+
+    def balance_tree(self):
+        # parcourir l'arbre
+        # trier la liste
+        # chaque parent est le milieu de la liste
         pass
 
-    def search_node(self):
-        pass
+    def search_node(self, node_value, return_node=False):
+        """
+        Search the node in the tree
+        :param int node_value: the value of the node to search
+        :param bool return_node: return the node in addition if he's found, and if this param is True
+        :return: False if not found, True otherwise
+        """
+        if self.current_node is None:
+            self.current_node = self.root
+            return False
 
-    def is_leaf(self, node):
-        return self.root.l_subtree is None and self.root.r_subtree is None
+        elif self.current_node.value == node_value:
+            if return_node:
+                node_package = (True, self.current_node)
+                self.current_node = self.root
+                return node_package
+
+            return True
+
+        elif self.current_node.value > node_value:
+            self.current_node = self.current_node.l_subtree
+            return self.search_node(node_value, return_node)
+
+        elif self.current_node.value < node_value:
+            self.current_node = self.current_node.r_subtree
+            return self.search_node(node_value, return_node)
+
+    def is_leaf(self, node_value):
+        """
+        Check if the node have subtrees (give value of the node)
+        :param int node_value: the value of the node to check | Value need to be in the tree
+        :return: False if not leaf, True otherwise
+        """
+        node = self.search_node(node_value, True)[1]
+        return node.l_subtree is None and node.r_subtree is None
 
     def __str__(self):
         return self.root.__str__()
@@ -59,10 +108,18 @@ class TraversingTree:
 
 
 if __name__ == '__main__':
-    abr = Tree(Node(5))
-    abr.insert_node(Node(3))
-    abr.insert_node(Node(6))
-    abr.insert_node(Node(2))
-    abr.insert_node(Node(7))
-    abr.insert_node(Node(4))
+    abr = Tree(Node(10))
+
+    for val in [16, 6, 8, 20, 1]:
+        abr.insert_node(Node(val))
+
+    print(abr)
+
+    print("16 present: ", abr.search_node(16))  # True
+    print("21 present: ", abr.search_node(21))  # False
+
+    print("16 leaf:", abr.is_leaf(16))  # False
+    print("1 leaf:", abr.is_leaf(1))  # True
+
+    abr.delete_node(1)
     print(abr)
